5256eb5442949cb50a14b1995761d7b09a28ab94
Chau Tran <chau.t.tran.us@gmail.com>
2023-05-14 00:26:57 -0400

    SQL setup scripts

--- sql/install.sql
+++ sql/install.sql
@@ -1,16 +1,23 @@
 CREATE TABLE entries (
-  id int NOT NULL,
-  word varchar(255),
-  pos varchar(255),
-  def varchar(2040),
+  id int NOT NULL AUTO_INCREMENT,
+  word VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+  pos VARCHAR(256),
+  def VARCHAR(4096),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE sentences (
-  id int NOT NULL,
+  id int NOT NULL AUTO_INCREMENT,
   entry_id int NOT NULL,
-  eng varchar(1020),
-  viet varchar(1020),
+  eng VARCHAR(4096),
+  viet VARCHAR(4096) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
   PRIMARY KEY (id),
   FOREIGN KEY(entry_id) REFERENCES entries(id)
+);
+
+CREATE TABLE text_audio (
+  id int NOT NULL AUTO_INCREMENT,
+  text VARCHAR(4096) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin UNIQUE,
+  audio_url VARCHAR(1024),
+  PRIMARY KEY(id)
 );
---
+++ sql/nuke.sql
@@ -0,0 +1,5 @@
+DROP TABLE sentences;
+
+DROP TABLE entries;
+
+DROP TABLE text_audio;
